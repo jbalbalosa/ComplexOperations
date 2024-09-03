750bf7035acad52167fafc86051cb92a65222662
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-03 11:34:49 +0900

    Update ComplexNumber.java

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,4 +1,39 @@
 package PACKAGE_NAME;
 
 public class ComplexNumber {
+    private double real = 0;
+    private double imaginary = 0;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary){
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber){
+        this.real += complexNumber.real;
+        this.imaginary += complexNumber.imaginary;
+    }
+
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber){
+        this.real -= complexNumber.real;
+        this.imaginary -= complexNumber.imaginary;
+    }
 }
